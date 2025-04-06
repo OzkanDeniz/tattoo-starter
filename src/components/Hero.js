690a32039f82402c6cb6788825b6bf6156223c3f
ozkan <ozkandnz09@gmail.com>
2025-04-06 23:00:03 +0300

    tatto starter

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,16 @@
 import React from 'react';
+//import hero data 
+import {heroData} from "../data"
+//import motion
+import {motion} from "framer-motion"
+//import fadeIn
+import {fadeIn} from "../variants"
+
 
 const Hero = () => {
-  return <div>Hero</div>;
+  //destructure herdata
+  const {title,subtitle, btnText,btnIcon} = heroData
+  return <div className='bg-pink-200'>Hero</div>;
 };
 
 export default Hero;
