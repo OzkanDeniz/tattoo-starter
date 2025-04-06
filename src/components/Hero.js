8f8118af47db2ab11db79d3556b00349b7e4805e
ozkan <ozkandnz09@gmail.com>
2025-04-06 23:40:51 +0300

    motion

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -30,14 +30,14 @@ const Hero = () => {
         {/* text & btn */}
         <div className="text-white text-center lg:text-left lg:max-w-[640px]">
           {/* text */}
-          <h1 className="h1">{title}</h1>
-          <p className="mb-8 lg:mb-16 max-w-lg leading-relaxed">{subtitle}</p>
+          <motion.h1 variants={fadeIn("down")} className="h1">{title}</motion.h1>
+          <motion.p variants={fadeIn("down")} className="mb-8 lg:mb-16 max-w-lg leading-relaxed">{subtitle}</motion.p>
           {/* btn */}
-          <div>
+          <motion.div variants={fadeIn("down")}>
             <button className="btn btn-sm lg:btn-lg btn-outline mx-auto lg:mx-0">
               {btnText} <div className="text-xl">{btnIcon}</div>
             </button>
-          </div>
+          </motion.div>
         </div>
         {/* outline text */}
         <div className="hidden xl:flex absolute -bottom-2 right-0 left-0 before:content-outlineText"></div>
