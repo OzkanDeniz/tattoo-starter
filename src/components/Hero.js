92a914d81bc6636b212a9a2affcca9ea254a2153
ozkan <ozkandnz09@gmail.com>
2025-04-06 23:34:24 +0300

    container

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,21 +6,31 @@ import { motion } from "framer-motion";
 //import fadeIn
 import { fadeIn } from "../variants";
 
+const container = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.6,
+    },
+  },
+};
+
 const Hero = () => {
   //destructure herdata
   const { title, subtitle, btnText, btnIcon } = heroData;
   return (
     <section className="bg-hero bg-cover bg-center min-h-[40vh] lg:h-[948px] bg-no-repeat relative mt-[120px] lg:mt-[150px] ">
-      <div className="container mx-auto min-h-[40vh] lg:h-full flex items-center justify-center xl:justify-end ">
+      <div  className="container mx-auto min-h-[40vh] lg:h-full flex items-center justify-center xl:justify-end ">
         {/* text & btn */}
-        <div className="text-white">
+        <div className="text-white text-center lg:text-left lg:max-w-[640px]">
           {/* text */}
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
+          <h1 className="h1">{title}</h1>
+          <p className="mb-8 lg:mb-16 max-w-lg leading-relaxed">{subtitle}</p>
           {/* btn */}
           <div>
-            <button>
-              {btnText} <div>{btnIcon}</div>
+            <button className="btn btn-sm lg:btn-lg btn-outline mx-auto lg:mx-0">
+              {btnText} <div className="text-xl">{btnIcon}</div>
             </button>
           </div>
         </div>
