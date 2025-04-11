76dfaff2d369aabf2d0f67296faeff16b19e40dd
ozkan <ozkandnz09@gmail.com>
2025-04-11 13:43:02 +0300

    state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
-import { useEffect ,useState} from "react";
+import { useEffect, useState } from "react";
 // import header data
 import { headerData } from "../data";
 // import components
-import Nav from "./Nav"
-import NavMobile from "./NavMobile"
-import Social from "./Socials"
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+import Social from "./Socials";
 // import icons
-import {TiThMenuOutline} from "react-icons/ti"
+import { TiThMenuOutline } from "react-icons/ti";
 
 const Header = () => {
+  // destructure header data
+  const { logo } = headerData;
+  // header state 
+  const [isActive, setIsActive] = useState(false)
   return <div>Header</div>;
 };
 
