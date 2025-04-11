d7010bf7e538a391d030aa09afac36b3e1d4617c
ozkan <ozkandnz09@gmail.com>
2025-04-11 15:53:11 +0300

    map

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import { navData } from "../data";
+import Socials from "./Socials";
 
 const NavMobile = () => {
-  return <div>NavMobile</div>;
+  const { items } = navData;
+  return (
+    <nav className="w-full h-full flex flex-col bg-green-500 justify-evenly overflow-hidden">
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            <a href={item.href}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavMobile;
