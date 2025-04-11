ad88c4b693f7aaa9d7c8eacd2e16f4bc883b5e28
ozkan <ozkandnz09@gmail.com>
2025-04-11 14:45:02 +0300

    class isactive

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,15 @@ const Header = () => {
   const { logo } = headerData;
   // header state 
   const [isActive, setIsActive] = useState(false)
-  return <div>Header</div>;
+  // nav mobile state
+  const [navMobile, setNavMobile] = useState(false)
+  // scroll event
+  useEffect(()=>{
+    window.addEventListener("scroll", ()=>{
+      window.scrollY > 50 ? setIsActive(true) : setIsActive(false)
+    })
+  })
+  return <header className={`${isActive ? "bg-red-500" : "bg-green-500"} fixed left-0 right-0 top-0 z-10`}>Header</header>;
 };
 
 export default Header;
